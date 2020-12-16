a1b1c79bb9c5783007dce98173c15b5ee2cbd1f7
Tunnelvision77 <Tacomablues666@gmail.com>
2020-12-16 16:26:19 -0700

    changes

--- Setup.sql
+++ Setup.sql
@@ -1,5 +1,4 @@
 -- DROP TABLE profiles
-
 -- CREATE TABLE profiles
 -- (
 --   id VARCHAR(255) NOT NULL,
@@ -8,57 +7,51 @@
 --   picture VARCHAR(255),
 --   PRIMARY KEY(id)
 -- )
-SELECT * FROM vaultkeeps
--- DROP TABLE keeps
-
--- CREATE TABLE keeps
--- (
---   id INT AUTO_INCREMENT,
---   creatorId VARCHAR(255) NOT NULL,
---   name VARCHAR(255) NOT NULL,
---   description VARCHAR(255) NOT NULL,
---   img VARCHAR(255) NOT NULL,
---   views INT,
---   shares INT,
---   keeps INT,
---   PRIMARY KEY(id),
-
---   FOREIGN KEY (creatorId)
---     REFERENCES profiles (id)
---     ON DELETE CASCADE
--- )
-
--- CREATE TABLE vaults
--- (
---   id INT AUTO_INCREMENT,
---   creatorId VARCHAR(255) NOT NULL,
---   name VARCHAR(255) NOT NULL,
---   description VARCHAR(255) NOT NULL,
---   isPrivate TINYINT,
---   PRIMARY KEY(id),
-
---   FOREIGN KEY (creatorId)
---     REFERENCES profiles (id)
---     ON DELETE CASCADE
--- )
-
--- CREATE TABLE vaultkeeps
--- (
---   id INT AUTO_INCREMENT,
---   creatorId VARCHAR(255) NOT NULL,
---   vaultId INT,
---   keepId INT,
---   PRIMARY KEY(id),
-
---   FOREIGN KEY (creatorId)
---     REFERENCES profiles (id)
---     ON DELETE CASCADE,
-
---   FOREIGN KEY (vaultId)
---     REFERENCES vaults (id)
---     ON DELETE CASCADE,
-
---   FOREIGN KEY (keepId)
---     REFERENCES keeps (id)
---     ON DELETE CASCADE
--- );
+SELECT
+  *
+FROM
+  vaultkeeps -- DROP TABLE keeps
+  -- CREATE TABLE keeps
+  -- (
+  --   id INT AUTO_INCREMENT,
+  --   creatorId VARCHAR(255) NOT NULL,
+  --   name VARCHAR(255) NOT NULL,
+  --   description VARCHAR(255) NOT NULL,
+  --   img VARCHAR(255) NOT NULL,
+  --   views INT,
+  --   shares INT,
+  --   keeps INT,
+  --   PRIMARY KEY(id),
+  --   FOREIGN KEY (creatorId)
+  --     REFERENCES profiles (id)
+  --     ON DELETE CASCADE
+  -- )
+  -- CREATE TABLE vaults
+  -- (
+  --   id INT AUTO_INCREMENT,
+  --   creatorId VARCHAR(255) NOT NULL,
+  --   name VARCHAR(255) NOT NULL,
+  --   description VARCHAR(255) NOT NULL,
+  --   isPrivate TINYINT,
+  --   PRIMARY KEY(id),
+  --   FOREIGN KEY (creatorId)
+  --     REFERENCES profiles (id)
+  --     ON DELETE CASCADE
+  -- )
+  -- CREATE TABLE vaultkeeps
+  -- (
+  --   id INT AUTO_INCREMENT,
+  --   creatorId VARCHAR(255) NOT NULL,
+  --   vaultId INT,
+  --   keepId INT,
+  --   PRIMARY KEY(id),
+  --   FOREIGN KEY (creatorId)
+  --     REFERENCES profiles (id)
+  --     ON DELETE CASCADE,
+  --   FOREIGN KEY (vaultId)
+  --     REFERENCES vaults (id)
+  --     ON DELETE CASCADE,
+  --   FOREIGN KEY (keepId)
+  --     REFERENCES keeps (id)
+  --     ON DELETE CASCADE
+  -- );
